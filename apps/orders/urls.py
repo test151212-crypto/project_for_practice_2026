492a735dbda3ae19501194a2659a26d348601cac
Saveliy <test151212@gmail.com>
2026-06-06 06:29:14 +0500

    Update urls.py

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import OrderViewSet, CategoryViewSet
 
 router = DefaultRouter()
-router.register('orders', OrderViewSet)
+router.register('orders', OrderViewSet, basename='order')
 router.register('categories', CategoryViewSet)
 
 urlpatterns = [
